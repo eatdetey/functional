962db45f6772eb2631c6808d797175e57552d064
eatdetey <vsevozmoznoe@mail.ru>
2025-03-16 20:30:21 +0300

    L1 T4&5 Recursion down&up

---
+++ Hello from F#/digits.fsx
@@ -0,0 +1,16 @@
+let rec digits_Sum n =
+    if n=0 then 0
+    else (n%10) + (digits_Sum(n/10))
+
+let digits_Sum_down (n:int) =
+    let rec digits_Sum_down1 n current_sum =
+        if n = 0 then current_sum
+        else
+            let n1=n/10
+            let digit = n%10
+            let new_sum = current_sum + digit
+            digits_Sum_down1 n1 new_sum
+    digits_Sum_down1 n 0
+
+System.Console.WriteLine(digits_Sum(123))
+System.Console.WriteLine(digits_Sum_down(123))
