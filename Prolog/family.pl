01bacb9556175bc81d8057ca6a1cc47ae69e5ea2
eatdetey <vsevozmoznoe@mail.ru>
2025-04-14 19:09:04 +0300

    L8 T2 New predicates

--- Prolog/family.pl
+++ Prolog/family.pl
@@ -4,6 +4,7 @@ man(alex).
 man(david).
 man(saur).
 man(kirill).
+man(denis).
 
 woman(mary).
 woman(helena).
@@ -14,9 +15,12 @@ woman(larila).
 married(andrey, mary).
 married(ervand, helena).
 married(alex, tatyana).
+married(kirill, larila).
 
 parent(andrey, david).
 parent(mary, david).
+parent(ervand, denis).
+parent(helena, denis).
 parent(ervand, anna).
 parent(helena, anna).
 parent(ervand, larila).
@@ -38,16 +42,39 @@ children(X) :- parent(X, Y), write(Y), nl, fail.
 children(_).
 
 mother(X, Y) :- woman(X), parent(X, Y).
-
-mother(X) :- mother(Y, X), write(Y), nl, fail.
-mother(_).
+mother(X) :- mother(Y, X), write(Y).
 
 brother(X, Y) :- man(X), parent(Z, X), parent(Z, Y), X \= Y.
 
-brothers(X) :- brother(Y, X), write(Y), nl, fail.
+brothers(X) :- setof(Y, brother(Y, X), L), write_list(L).
 brothers(_).
 
 b_s(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
 
-b_s(X) :- b_s(Y, X), write(Y), nl, fail.
+b_s(X) :- setof(Y, b_s(Y, X), L), write_list(L).
 b_s(_).
+
+father(X,Y) :- man(X), parent(X, Y).
+father(X) :- father(Y, X), write(Y).
+
+sister(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y, woman(X).
+
+sisters(X) :- setof(Y, sister(Y, X), L), write_list(L).
+sisters(_).
+
+grand_so(X, Y) :- man(X), parent(Y, Z), parent(Z, X).
+
+grand_sons(X) :- setof(Y, grand_so(Y, X), L), write_list(L).
+grand_sons(_).
+
+grand_pa_and_son(X, Y) :- grand_so(Y, X) ; grand_so(X, Y).
+
+uncle(X, Y) :- man(X), parent(Z, Y), parent(Z, W), brother(X, W).
+
+nephews(X) :- setof(Y, uncle(X, Y), L), write_list(L).
+nephews(_).
+
+write_list([]).
+write_list([H|T]) :-
+    write(H), nl,
+    write_list(T).
