fca6e1280c2abc4e9b08fd35f0f2d9878bd9e33c
eatdetey <vsevozmoznoe@mail.ru>
2025-04-14 20:01:58 +0300

    L8 T4 Akinator Lite updated with Java and Haskell

--- Prolog/akynator_lite.pl
+++ Prolog/akynator_lite.pl
@@ -1,17 +1,14 @@
-:-dynamic high/2.
-read_str(A):-get0(X),r_str(X,A,[]).
-r_str(10,A,A):-!.
-r_str(X,A,B):-append(B,[X],B1),get0(X1),r_str(X1,A,B1).
-high_r(X,Y):-	repeat, (high(X,Y) -> (put(32),write(X),nl,write(Y),write("."),nl,
-				retract(high(X,Y))) ;X=nil,Y=nil).
-pr2:-tell('c:/Prolog/29_1_prolog_F/111.txt'),high_r(X,_),X=nil,told.
-pr3:-see('c:/Prolog/29_1_prolog_F/111.txt'),get0(Sym),read_high(Sym),seen.
-read_high(-1):-!.
-read_high(_):-	read_str(Lang),name(X,Lang),read(Y),asserta(high(X,Y)),
-				get0(Sym),read_high(Sym).
+high(ruby,1).
+high(python,1).
+high(c_sharp,1).
+high(c_plu_plus,1).
+high(f_sharp,1).
+high(prolog,1).
+high(c,0).
+high(asm,0).
+high(haskell,1).
+high(java,1).
 
-
-	
 decl(ruby,0).
 decl(c_sharp,0).
 decl(python,0).
@@ -20,20 +17,26 @@ decl(f_sharp,1).
 decl(prolog,1).
 decl(c,0).
 decl(asm,0).
+decl(haskell,1).
+decl(java,0).
 
 interpret(ruby,1).
 interpret(python,1).
 interpret(f_sharp,1).
 interpret(prolog,1).
+interpret(haskell,1).
 interpret(c_sharp,0).
 interpret(c_plu_plus,0).
 interpret(c,0).
 interpret(asm,0).
+interpret(java,0).
 
 oop(ruby,3).
 oop(c_sharp,3).
+oop(java,3).
 oop(python,2).
 oop(c_plu_plus,2).
+oop(haskell,1).
 oop(f_sharp,1).
 oop(prolog,1).
 oop(c,0).
@@ -45,6 +48,8 @@ cross(c_plu_plus,1).
 cross(prolog,1).
 cross(c,1).
 cross(asm,1).
+cross(java,1).
+cross(haskell,1).
 cross(c_sharp,0).
 cross(f_sharp,0).
 
@@ -53,7 +58,9 @@ visual(ruby,2).
 visual(python,2).
 visual(c_plu_plus,2).
 visual(f_sharp,2).
+visual(java,2).
 visual(prolog,1).
+visual(haskell,1).
 visual(c,0).
 visual(asm,0).
 
@@ -65,6 +72,19 @@ mobile(f_sharp,0).
 mobile(prolog,0).
 mobile(c,0).
 mobile(asm,0).
+mobile(haskell,0).
+mobile(java,1).
+
+functional(ruby,0).
+functional(c_sharp,0).
+functional(python,0).
+functional(c_plu_plus,0).
+functional(f_sharp,1).
+functional(prolog,0).
+functional(c,0).
+functional(asm,0).
+functional(haskell,1).
+functional(java,0).
 
 question1(X1):-	write("Is your language high level?"),nl,
 				write("1. Yes"),nl,
@@ -105,12 +125,17 @@ question7(X7):-	write("Is your language for mobile phones?"),nl,
 				write("0. NO"),nl,
 				read(X7).				
 
+question8(X8):-	write("Is your language functional?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X8).
+
 
 
 pr:-	question1(X1),question2(X2),question3(X3),question4(X4),
-		question5(X5),question6(X6),question7(X7),
+		question5(X5),question6(X6),question7(X7), question8(X8),
 		high(X,X1),decl(X,X2),interpret(X,X3),oop(X,X4),
-		cross(X,X5),visual(X,X6),mobile(X,X7),
+		cross(X,X5),visual(X,X6),mobile(X,X7), functional(X,X8),
 		write(X).
 
 
