dec750a6d85fef6e61a812b711bf7d8b80e84e39
eatdetey <vsevozmoznoe@mail.ru>
2025-04-23 00:20:59 +0300

    L9 T5-6

--- Prolog/operations.pl
+++ Prolog/operations.pl
@@ -165,3 +165,90 @@ testTask3 :-
     swap_min_max(List,Min,Max,Result3),
     write('Смена местами максимального и минимального:'), nl,
     write_list(Result3).
+
+% Проверка на простоту
+prime(2).
+prime(N) :- N > 2, N mod 2 =\= 0, not(has_divisor(N,3)).
+
+% Проверка наличия делителя от D до sqrt(N)
+has_divisor(N,D) :- D*D =< N, (N mod D =:= 0 ; D2 is D+2, has_divisor(N,D2)).
+
+% Делитель числа
+divisor(X,Y) :- Y mod X =:= 0.
+
+% Сумма простых делителей
+sum_prime_divisors(N,Sum) :- sum_prime_divisors(N,2,0,Sum).
+
+sum_prime_divisors(N,Cur,Acc,Acc) :- Cur > N, !.
+sum_prime_divisors(N,Cur,Acc,Sum) :-
+    Cur =< N,
+    divisor(Cur,N),
+    prime(Cur), !,
+    Acc1 is Acc + Cur,
+    Cur1 is Cur + 1,
+    sum_prime_divisors(N,Cur1,Acc1,Sum).
+sum_prime_divisors(N,Cur,Acc,Sum) :-
+    Cur1 is Cur + 1,
+    sum_prime_divisors(N,Cur1,Acc,Sum).
+
+% Произведение делителей с условием
+product_divisors_by_digit_sum(N,Product) :- 
+    numSumDown(N,SumN),
+    product_divisors_by_digit_sum(N,2,SumN,1,Product).
+
+product_divisors_by_digit_sum(N,Cur,_,Product,Product) :- Cur > N, !.
+product_divisors_by_digit_sum(N,Cur,SumN,Acc,Product) :-
+    Cur =< N,
+    divisor(Cur,N),
+    numSumDown(Cur,SumC),
+    SumC < SumN, !,
+    Acc1 is Acc * Cur,
+    Cur1 is Cur + 1,
+    product_divisors_by_digit_sum(N,Cur1,SumN,Acc1,Product).
+product_divisors_by_digit_sum(N,Cur,SumN,Acc,Product) :-
+    Cur1 is Cur + 1,
+    product_divisors_by_digit_sum(N,Cur1,SumN,Acc,Product).
+
+% Подсчёт количества вхождений элемента
+count_elem(_,[],0).
+count_elem(Elem,[Elem|T],Count) :- count_elem(Elem,T,Count1), Count is Count1 + 1.
+count_elem(Elem,[_|T],Count) :- count_elem(Elem,T,Count).
+
+% Подсчёт количества минимальных элементов
+count_min(List,Count) :- 
+    min_list(List,Min),
+    count_elem(Min,List,Count).
+
+% Построить список уникальных элементов
+unique([],[]).
+unique([H|T],R) :- in_list(T,H), !, unique(T,R).
+unique([H|T],[H|R]) :- unique(T,R).
+
+% Удаление всех вхождений элемента
+remove_all(_,[],[]).
+remove_all(E,[E|T],R) :- remove_all(E,T,R).
+remove_all(E,[H|T],[H|R]) :- E \= H, remove_all(E,T,R).
+
+% Подсчёт количества вхождений элемента
+count_elem(_,[],0).
+count_elem(Elem,[Elem|T],Count) :- count_elem(Elem,T,Count1), Count is Count1 + 1.
+count_elem(Elem,[_|T],Count) :- count_elem(Elem,T,Count).
+
+% Построить список N одинаковых элементов
+make_list(_,0,[]).
+make_list(E,N,[E|T]) :- N > 0, N1 is N-1, make_list(E,N1,T).
+
+% Главный предикат
+sort_by_count([],[]).
+sort_by_count(List,Result) :-
+    max_count_elem(List,MaxEl,MaxCount),
+    make_list(MaxEl,MaxCount,L1),
+    remove_all(MaxEl,List,Rest),
+    sort_by_count(Rest,RestResult),
+    append(L1,RestResult,Result).
+
+% Найти элемент с максимальным количеством вхождений
+max_count_elem(List,MaxEl,MaxCount) :-
+    findall(Count-Elem, (in_list(List,Elem), count_elem(Elem,List,Count)), Pairs),
+    sort(0,@>=,Pairs,Sorted),  % сортируем по убыванию Count
+    Sorted = [MaxCount-MaxEl|_].
